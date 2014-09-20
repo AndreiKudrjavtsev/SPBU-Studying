8651487771bbf59c72f1b6b90696a4d17d21cb2c
Kudryavtsev Andrey <tmac1attack@gmail.com>
2014-09-20 04:31:02 +0400

    task10

---
+++ HW1/task10/task10/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream> 
+#include "time.h"
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+
+	int length = 0;
+	int range = 0;
+	cout << "Enter the length of the array: " << endl;
+	cin >> length;
+	cout << "Enter the range of randomed values of array (from 0 to your number): " << endl;
+	cin >> range;
+
+	int *arr = new int[length];
+	int amountOfNulls = 0; 
+	cout << "Your array: " << endl;
+	for (int i = 0; i < length; i++)
+	{
+		arr[i] = rand() % range;
+		cout << arr[i] << " ";
+		if (arr[i] == 0)
+			amountOfNulls++;
+	}
+	cout << endl << "Amount of nulls in this array is: " << amountOfNulls << endl;
+
+	return 0;
+}
