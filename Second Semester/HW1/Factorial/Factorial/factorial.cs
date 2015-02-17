6a4a564b52352e69a05a71ea0cc04f663ccd5a74
Kudryavtsev Andrey <tmac1attack@gmail.com>
2015-02-17 17:15:57 +0300

    HW1, factorial

---
+++ Second Semester/HW1/Factorial/Factorial/factorial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HW1
+{
+    class Factorial
+    {
+        public static int factorial(int number)
+        {
+            if (number < 0)
+                return 0;
+            if (number == 0 || number == 1)
+                return 1;
+            else
+            {
+                int result = 1;
+                for (int i = 1; i <= number; i++)
+                {
+                    result *= i;
+                }
+                return result;
+            }
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Enter the number: ");
+            int number = int.Parse(Console.ReadLine());
+            int result = factorial(number);
+            Console.WriteLine("Your result: " + result);
+        }
+    }
+}
