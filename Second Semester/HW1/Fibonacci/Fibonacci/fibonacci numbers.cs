0fc8bade44dec34a298dd36b506af919a794d1ca
Kudryavtsev Andrey <tmac1attack@gmail.com>
2015-02-17 17:16:13 +0300

    HW1, Fibonacci

---
+++ Second Semester/HW1/Fibonacci/Fibonacci/fibonacci numbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HW1
+{
+    class FibonacciNumbers
+    {
+        public static int iterFibNums(int number)
+        {
+            int fibNum1 = 0;
+            int fibNum2 = 1;
+            int newFib = 0;
+            if (number <= 1)
+                return number;
+            else
+            {
+                for (int i = 1; i < number; ++i)
+                {
+                    newFib = fibNum1 + fibNum2;
+                    fibNum1 = fibNum2;
+                    fibNum2 = newFib;
+                }
+                return newFib;
+            }
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Enter the number: ");
+            int number = int.Parse(Console.ReadLine());
+            int result = iterFibNums(number);
+            Console.WriteLine("Your result: " + result);
+        }
+    }
+}
