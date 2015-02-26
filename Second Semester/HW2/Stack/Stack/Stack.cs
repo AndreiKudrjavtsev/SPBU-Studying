f89f35f4346e0a3bd72c809968e116fde34dd41c
Кудрявцев Андрей <tmac1attack@gmail.com>
2015-02-26 18:02:05 +0400

    Stack, HW2
    beginning of stack, incompleted structure

---
+++ Second Semester/HW2/Stack/Stack/Stack.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HW2
+{
+    public class Stack<Type>
+    {
+        private StackElement head;
+        private int length;
+
+        private class StackElement
+        {
+            /// <summary>
+            /// value of stack element 
+            /// </summary>
+            public Type value { get; set; }
+            /// <summary>
+            /// link to the next stack element
+            /// </summary>
+            public StackElement next { get; set; }
+        }
+
+        
+
+    }
+}
