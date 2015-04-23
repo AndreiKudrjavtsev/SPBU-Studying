dbcf12e7116d62c6763307ac378f8ee4efd43a8e
Kudryavtsev Andrey <tmac1attack@gmail.com>
2015-04-23 17:08:20 +0300

    classes only

---
+++ Second Semester/HW2/Stack Calculator/Stack Calculator/ArrayStack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using HW2_StackCalculator;
+
+namespace HW2_ArrayStack
+{
+    class ArrayStack<Type> : IStack<Type>
+    {
+        private int lastElementIndex;
+        private Type[] stack;
+
+        public ArrayStack(int size)
+        {
+            lastElementIndex = 0;
+            stack = new Type[size];
+        }
+
+        public void Push(Type value)
+        {
+            if (lastElementIndex == 0)
+                Array.Resize(ref stack, stack.Length * 2);
+            stack[lastElementIndex] = value;
+            lastElementIndex++;
+        }
+
+        public void Pop()
+        {
+            if (lastElementIndex == 0)
+                throw new InvalidOperationException("Stack is empty!");
+            lastElementIndex--;
+        }
+
+        public bool IsEmpty()
+        {
+            return lastElementIndex == 0;
+        }
+
+        public Type Peek()
+        {
+            if (lastElementIndex == 0)
+                throw new InvalidOperationException("Stack is empty!");
+            return stack[lastElementIndex];
+        }
+
+        public void PrintStack()
+        {
+            for (int i = lastElementIndex; i >= 0; i--)
+                Console.Write("{0} ", stack[i]);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Second Semester/HW2/Stack Calculator/Stack Calculator/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HW2_StackCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
