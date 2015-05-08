30c7e58074d02d8b910dc3afd9e1a1931101f005
Kudryavtsev Andrey <tmac1attack@gmail.com>
2015-05-08 16:55:05 +0300

    indicator with buttons

---
+++ Second Semester/CW1/Control/Control/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Control
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int currValue = progressBar1.Value + progressBar1.Step;
+
+            if (currValue >= progressBar1.Maximum)
+            {
+                progressBar1.Value = progressBar1.Maximum;
+                button2.Visible = true;
+            }
+            else
+            {
+                progressBar1.Value = currValue;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            button1.Text = "Processing...";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
