a9f0bb00c5b59aeec19b021d95358d584e4625ab
Кудрявцев Андрей <tmac1attack@gmail.com>
2015-05-12 00:38:15 +0400

    added map function with List<T>

---
+++ Second Semester/HW5/MapFunction/MapFunction/Map.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MapFunction
+{
+    public static class Map
+    {
+        List<T> MapFunction<T>(List<T> list, Func<T, T> function)
+        {
+            List<T> newList = new List<T>();
+            foreach (T value in list)
+            {
+                newList.Add(function(value));
+            }
+            return newList;
+        }
+    }
+}
---
+++ Second Semester/HW5/MapFunction/MapFunction/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MapFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
