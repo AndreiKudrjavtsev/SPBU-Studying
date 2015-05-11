4c1d6c6605bbae293a91936756b71cbf42bdd216
Кудрявцев Андрей <tmac1attack@gmail.com>
2015-05-11 23:12:01 +0400

    Added generic stack and list w/o tests

---
+++ Second Semester/HW7/GenericClasses/GenericClasses/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using GenericClasses;
+
+namespace GenericClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var stack = new GenericStack<int>();
+            //stack.Push(1);
+            //stack.Push(2);
+            //stack.Push(3);
+            //var newStack = new GenericStack<int>();
+            //foreach (int value in stack)
+            //{
+            //    newStack.Push(value * 3);
+            //}
+            //newStack.PrintStack();
+        }
+    }
+}
