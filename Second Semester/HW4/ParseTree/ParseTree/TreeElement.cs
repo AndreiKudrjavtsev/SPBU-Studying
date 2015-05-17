314fd103d55ee364c8061111176820cc044b9c93
Kudryavtsev Andrey <tmac1attack@gmail.com>
2015-05-17 15:37:01 +0300

    class hierarchy

---
+++ Second Semester/HW4/ParseTree/ParseTree/TreeElement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseTree
+{
+    abstract class TreeElement
+    {
+        public abstract string PrintTree();
+        public abstract int CalculateTree();
+        public abstract bool AdditionIsAvailable();
+        public abstract void AddElement();
+    }
+}
