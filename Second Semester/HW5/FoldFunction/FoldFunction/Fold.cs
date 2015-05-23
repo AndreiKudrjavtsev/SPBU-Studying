16827ad821811e38699cedf2859f70cc8ae276fa
Kudryavtsev Andrey <tmac1attack@gmail.com>
2015-05-23 22:49:36 +0300

    added tests

--- Second Semester/HW5/FoldFunction/FoldFunction/Fold.cs
+++ Second Semester/HW5/FoldFunction/FoldFunction/Fold.cs
@@ -5,7 +5,7 @@ namespace FoldFunction
 {
     public static class Fold
     {
-        T FoldFunction<T>(List<T> list, T initialValue, Func<T, T, T> function)
+        public static T FoldFunction<T>(List<T> list, T initialValue, Func<T, T, T> function)
         {
             foreach (T value in list)
                 initialValue = function(initialValue, value);
