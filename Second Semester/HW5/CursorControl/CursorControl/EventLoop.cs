6efeba1cd007f9e4493c518d2f0f35d4ed81f812
Kudryavtsev Andrey <tmac1attack@gmail.com>
2015-05-28 14:26:00 +0300

    Cursor control app

---
+++ Second Semester/HW5/CursorControl/CursorControl/EventLoop.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursorControl
+{
+    class EventLoop
+    {
+        public delegate void ArrowHandler();
+
+        public void Run(ArrowHandler left, ArrowHandler right, ArrowHandler up, ArrowHandler down)
+        {
+            while (true)
+            {
+                var key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        left();
+                        break;
+                    case ConsoleKey.RightArrow:
+                        right();
+                        break;
+                    case ConsoleKey.UpArrow:
+                        up();
+                        break;
+                    case ConsoleKey.DownArrow:
+                        down();
+                        break;
+                }
+            }
+        }
+    }
+}
