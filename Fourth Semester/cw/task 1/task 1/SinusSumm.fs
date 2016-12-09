31954a462facfc64f36e24c1d08820a9b4d2e676
Кудрявцев Андрей <tmac1attack@gmail.com>
2016-12-09 16:25:20 +0300

    task 1&2 of cw

---
+++ Fourth Semester/cw/task 1/task 1/SinusSumm.fs
@@ -0,0 +1,8 @@
+﻿let rec summSin list = 
+    let rec summing lst (sum: float) = 
+        match lst with 
+        | h::t -> summing t (sum + sin(h))
+        | [] -> sum
+    summing list 0.0
+
+printfn "%A" (summSin [3.14])
---
+++ Fourth Semester/cw/task2/task2/RhombusPrint.fs
@@ -0,0 +1,20 @@
+﻿let printRhombus n = 
+    let size = 2 * n - 1
+    let mutable currSize = 1
+    let mutable spacesAmount = 0
+    // пока не дошли до середины - false, потом меняем на true.
+    let mutable middleFlag = false
+    for i = 1 to size do
+        if i > size / 2 then middleFlag <- true
+        if not middleFlag then 
+            spacesAmount <- (size - currSize) / 2
+            (String.init spacesAmount (fun i -> " ") + String.init currSize (fun i -> "*") 
+                + String.init spacesAmount (fun i -> " ")) |> printfn "%s" 
+            currSize <- currSize + 2
+        elif middleFlag then 
+            spacesAmount <- (size - currSize) / 2
+            (String.init spacesAmount (fun i -> " ") + String.init currSize (fun i -> "*") 
+                + String.init spacesAmount (fun i -> " ")) |> printfn "%s" 
+            currSize <- currSize - 2
+
+printRhombus 3
