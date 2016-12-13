1cb43c113a48cd4ef5f2cd5f0480ef85e9a1def7
Кудрявцев Андрей <tmac1attack@gmail.com>
2016-12-13 22:00:15 +0300

    2nd homework

---
+++ Fourth Semester/HW2/NumbersMultiplication/NumbersMultiplication/Program.fs
@@ -0,0 +1,12 @@
+﻿let numbersMulti number = 
+    let rec multiCount value acc = 
+        if value > 9 
+        then multiCount (value / 10) (acc * (value % 10))
+        else acc * (value % 10)
+    multiCount number 1
+
+printfn "%A" (numbersMulti 1234)
+printfn "%A" (numbersMulti 100)
+printfn "%A" (numbersMulti 333)
+printfn "%A" (numbersMulti 0)
+printfn "%A" (numbersMulti 1239859)
