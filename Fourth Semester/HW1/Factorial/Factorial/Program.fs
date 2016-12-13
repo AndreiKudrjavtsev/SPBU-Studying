e610ddec19bd47b9cc7c34325c1fe2676da88a41
Кудрявцев Андрей <tmac1attack@gmail.com>
2016-12-13 21:57:42 +0300

    1st homework

---
+++ Fourth Semester/HW1/Factorial/Factorial/Program.fs
@@ -0,0 +1,7 @@
+﻿let rec factorial n =
+  match n with 
+  | 0 | 1 -> 1 
+  | _ -> n * factorial (n - 1)
+
+printfn "factorial of 0: %A" (factorial 0)
+printfn "factorial of 5: %A" (factorial 5)
---
+++ Fourth Semester/HW1/PowersOfTwo/PowersOfTwo/Program.fs
@@ -0,0 +1,11 @@
+﻿let powersOfTwo powersRangeStart = 
+  let rec formList list currentPower iter = 
+    if iter <> 5 then 
+      let list = list @ [(pown 2 currentPower)] 
+      formList list (currentPower + 1) (iter + 1)
+    else 
+      list
+  formList [] powersRangeStart 0
+
+printfn "%A" (powersOfTwo 0)
+printfn "%A" (powersOfTwo 5)
