23890aaf0c56dc0b9a262e5a4751769818adffff
Кудрявцев Андрей <tmac1attack@gmail.com>
2016-12-15 20:05:48 +0300

    6th homework

---
+++ Fourth Semester/HW6/RoundingWorkflow/RoundingWorkflow/Program.fs
@@ -0,0 +1,23 @@
+﻿type RoundingBuilder(n: int) = 
+    member this.Bind(x: float, f: float -> float) =
+        System.Math.Round(f x, n)
+    member this.Return(x) = 
+        x
+
+let rounding n = RoundingBuilder n
+
+let test = 
+    rounding 3 {
+        let! a = 2.0 / 12.0
+        let! b = 3.5
+        return a / b
+    }
+    |> printfn "%A"
+
+let test2 = 
+    rounding 5 {
+        let! a = 1.0
+        let! b = 6.0
+        return a / b
+    }
+    |> printfn "%A"
---
+++ Fourth Semester/HW6/StringExpBuilder/StringExpBuilder/Program.fs
@@ -0,0 +1,31 @@
+﻿let findNumber str = 
+    match System.Double.TryParse(str) with 
+    | true, num -> Some(num)
+    | false, _ -> None
+
+type StrExpBuilder() = 
+    member this.Bind(x, f) = 
+        match findNumber x with 
+        | Some num -> f num
+        | None -> None
+    member this.Return(x) = 
+        Some x
+        
+let strexpr = StrExpBuilder()
+
+strexpr {
+    let! x = "1"
+    let! y = "2"
+    let z = x + y
+    return z
+}
+|> printfn "%A"
+
+
+strexpr {
+        let! x = "1"
+        let! y = "Ъ"
+        let z = x + y
+        return z
+    }
+|> printfn "%A"
