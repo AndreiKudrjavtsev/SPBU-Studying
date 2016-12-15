4b26887391942d3204a952b1964792ac4562f561
Кудрявцев Андрей <tmac1attack@gmail.com>
2016-12-15 20:48:30 +0300

    fix

--- Fourth Semester/HW2/FirstEntry/FirstEntry/Program.fs
+++ Fourth Semester/HW2/FirstEntry/FirstEntry/Program.fs
@@ -1,9 +1,9 @@
 ﻿let firstEntryInList list number = 
     let rec firstEntry lst value pos = 
         match lst with 
-        | h :: t when h = value -> pos
+        | h :: t when h = value -> Some pos
         | h :: t when h <> value -> firstEntry t value (pos + 1)
-        | _ -> -1
+        | _ -> None
     firstEntry list number 0
 
 printfn "%A" (firstEntryInList [0; 1; 2; 3; 4; 5] 3)
